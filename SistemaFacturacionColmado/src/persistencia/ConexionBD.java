90a85ad7a50bffc48f73891fd4c5a81383ffe4c6
1xX-Shadow-Xx1 <kevinliriano200816@gmail.com>
2025-08-07 09:35:17 -0400

    Prueba de correccion

--- SistemaFacturacionColmado/src/persistencia/ConexionBD.java
+++ SistemaFacturacionColmado/src/persistencia/ConexionBD.java
@@ -1,50 +1,8 @@
-<<<<<<< HEAD
+
 
 package persistencia;
 
-    import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-public class ConexionBD {
-
-    private static ConexionBD instancia;
-
-    private Connection conexion;
-
-    private final String URL = "jdbc:mysql://localhost:3306/colmadodatosgenerales"; 
-    private final String USUARIO = "root";
-    private final String CLAVE = "mysql123456"; 
-
-    private ConexionBD() {
-        try {
-            conexion = DriverManager.getConnection(URL, USUARIO, CLAVE);
-            System.out.println("Conectado a la base de datos.");
-        } catch (SQLException e) {
-            System.out.println("Error al conectar: " + e.getMessage());
-        }
-    }
-
-    public static ConexionBD getInstancia() {
-        if (instancia == null) {
-            instancia = new ConexionBD();
-        }
-        return instancia;
-    }
-
-    public Connection getConexion() {
-        return conexion;
-    }
-
-   
-}
-
-=======
-
-package persistencia;
-
-    import java.sql.Connection;
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -82,4 +40,4 @@ public class ConexionBD {
    
 }
 
->>>>>>> 0ebf63b5ed4eefef189dfe61a15519ace932a88f
+
