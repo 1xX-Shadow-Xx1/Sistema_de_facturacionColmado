a30506258cd4c3e9b1d1d7b1d8486feec62b9639
1xX-Shadow-Xx1 <kevinliriano200816@gmail.com>
2025-08-11 21:52:41 -0400

    859

---
+++ SistemaFacturacionColmado/src/Administrador/EmpleadoTemporal.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Administrador;
+
+/**
+ *
+ * @author super
+ */
+public class EmpleadoTemporal {
+    public String nombre, cedula, telefono, cargo;
+    public EmpleadoTemporal(String nombre, String cedula, String telefono, String cargo) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.telefono = telefono;
+        this.cargo = cargo;
+    }
+}
